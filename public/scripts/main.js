fc1486550316b1a63befe33e3fb20e5a3fbabcc7
Andrew Herculano <andrewherculano@hotmail.com>
2021-06-25 17:09:26 -0300

    configs modal

--- public/scripts/main.js
+++ public/scripts/main.js
@@ -5,20 +5,39 @@ const modal = Modal()
 const checkButtons = document.querySelectorAll('.actions a.check')
 const deleteButton  = document.querySelectorAll('.actions a.delete')
 
+const modalTitle = document.querySelector('.modal h2')
+const modalDescription = document.querySelector('.modal p')
+const modalButton = document.querySelector('.modal button')
+
+
+const handleClick = (event, check = true) => {
+  event.preventDefault()
+
+  const text = check ? 'Marcar como lida' : 'Excluir'
+
+  modalTitle.innerHTML = 
+    `${text} esta pergunta`
+    
+  modalDescription.innerHTML = 
+    `Tem certeza que deseja ${text.toLocaleLowerCase()} esta pergunta?`
+
+  modalButton.innerHTML = `Sim, ${text.toLocaleLowerCase()}`
+
+  check ? modalButton.classList.remove('red') : modalButton.classList.add('red')
+
+  modal.open()
+}
+
+//Abrir modal: Marcar como lido
 checkButtons.forEach(button => {
-  button.addEventListener('click', event => {
-    event.preventDefault()
-
-    modal.open()
-  })
+  button.addEventListener('click', handleClick)
 })
 
+//Abrir modal: Excluir
 deleteButton.forEach(button => {
-  button.addEventListener('click', event => {
-    event.preventDefault()
-
-    modal.open()
-  })
+  button.addEventListener('click', (event) => handleClick(event, false))
 })
+
+
 
 
