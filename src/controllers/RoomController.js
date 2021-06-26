73564d9a97badf97ce43491548750048942de55d
Andrew Herculano <andrewherculano@hotmail.com>
2021-06-26 10:30:41 -0300

    create table questions

--- src/controllers/RoomController.js
+++ src/controllers/RoomController.js
@@ -1,7 +1,42 @@
+const Database = require('../db/config')
+
 module.exports = {
-  create(req, res) {
-    let roomId = 123456
+  async create(req, res) {
+    const db = await Database()
+    const pass = req.body.password
+
+    let roomId
+    let isRoom = true
+
+    while (isRoom) {
+      for (var i = 0; i < 6; i++) {
+        i == 0 ? roomId = Math.floor(Math.random() * 10).toString() :
+          roomId += Math.floor(Math.random() * 10).toString()
+      }
+
+      /* Verificar se o id já existe no banco de dados */
+      const roomsExistIds = await db.all(`SELECT id FROM rooms`)
+
+      isRoom = roomsExistIds.some(roomExistId => roomExistId === roomId)
+
+      if (!roomsExistIds) {
+        await db.run(`INSERT INTO rooms (
+          id,
+          pass
+        ) VALUES (
+          ${parseInt(roomId)},
+          ${pass}
+        )`)
+      }
+    }
+
+    await db.close()
 
     res.redirect(`/room/${roomId}`)
+  },
+
+  open(req, res) {
+    const roomId = req.params.room
+    res.render('room', {roomId: roomId})
   }
 }
