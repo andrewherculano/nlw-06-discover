9d1e4431c9c733d3b5d4341bebebe9f4b35e6ecc
Andrew Herculano <andrewherculano@hotmail.com>
2021-06-26 12:51:56 -0300

    add no-questions

--- src/route.js
+++ src/route.js
@@ -9,6 +9,7 @@ route.get('/create-pass', (req, res) => res.render('index', {page: 'create-pass'
 
 route.get('/room/:room', RoomController.open)
 route.post('/create-room', RoomController.create)
+route.post('/enterroom', RoomController.enter)
 
 route.post('/question/create/:room', QuestionController.create)
 route.post('/question/:room/:question/:action', QuestionController.index)
